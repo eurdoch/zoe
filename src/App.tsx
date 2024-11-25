c5a9932b606f520a3d4c6b199fe2ab0e035a9b5a
George Balch <gbalch714@gmail.com>
2024-11-24 20:39:05 -0800

    Map datapoints to chart data

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { ScatterChart } from "@mui/x-charts";
 import "./App.css";
 import { useState } from "react";
 
-const data = [
-  { x: 100, y: 200, id: 1 },
-  { x: 120, y: 100, id: 2 },
-  { x: 170, y: 300, id: 3 },
-  { x: 140, y: 250, id: 4 },
-  { x: 150, y: 400, id: 5 },
-  { x: 110, y: 280, id: 6 },
-];
+interface DataPoint {
+  x: Date,
+  y: number,
+  id: number,
+}
+
+let data: DataPoint[] = [];
 
 function App() {
   const [showModal, setShowModal] = useState<boolean>(false);
@@ -36,10 +35,15 @@ function App() {
     <div className="flex flex-col gap-2 items-center p-4 w-screen h-screen">
       <div className="w-full h-1/3">
         <ScatterChart
-          series={[{ data }]}
-          xAxis={[{ min: 0, label: 'Date' }]}
+          series={ data.map((dataPoint) => ({
+            x: dataPoint.x.toString(),
+            y: dataPoint.y,
+            id: dataPoint.id,
+          }))}
+          xAxis={[{ label: 'Date' }]}
           yAxis={[{ label: 'Reps / lb' }]}
         />
+
       </div>
       <button onClick={handleLog}>{currentExercise ? currentExercise : "Choose Exercise" }</button>
       <div className="flex items-center gap-2">
