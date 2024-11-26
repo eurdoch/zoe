60df3c3fdcd728b45af932c125809dfe0f8f33d6
George Balch <gbalch714@gmail.com>
2024-11-25 21:32:39 -0800

    Use AWS url

--- src/exercises/network.ts
+++ src/exercises/network.ts
@@ -1,7 +1,9 @@
 import { Exercise } from "./types";
 
+const VITALE_BOX_URL = "18.237.137.224"
+
 export async function postExercise(exercise: Exercise) {
-  const response = await fetch('http://10.0.2.2/exercise', {
+  const response = await fetch(`http://${VITALE_BOX_URL}/exercise`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
