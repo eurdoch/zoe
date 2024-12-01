2ec69e904bd157d446679fed15b143229dea7582
George Balch <gbalch714@gmail.com>
2024-12-01 13:19:41 -0800

    Return mongodb result

--- api/routes/workoutRoutes.js
+++ api/routes/workoutRoutes.js
@@ -38,11 +38,7 @@ export default function workoutRoutes(workoutCollection) {
   router.delete('/:id', async (req, res) => {
     try {
       const result = await workoutCollection.deleteOne({ _id: new ObjectId(req.params.id) });
-      if (result.deletedCount === 1) {
-        res.status(204).send();
-      } else {
-        res.status(404).json({ error: 'Workout not found' });
-      }
+      res.status(200).json(result);
     } catch (err) {
       res.status(500).json({ error: 'Failed to delete workout' });
     }
