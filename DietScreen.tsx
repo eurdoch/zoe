c9b186e073047f11022fef245d73fc432acad328
George Balch <gbalch714@gmail.com>
2024-11-30 22:54:51 -0800

    Fix styling

--- DietScreen.tsx
+++ DietScreen.tsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import FoodEntry from './types/FoodEntry';
 import { getFoodByUnixTime } from './network/food';
+import FloatingActionButton from './FloatingActionButton';
+import DietLogScreen from './DietLogScreen';
 
 const DietScreen = () => {
   const [foodEntries, setFoodEntries] = useState<FoodEntry[]>([])
+  const [logActive, setLogActive] = useState<boolean>(false);
 
   useEffect(() => {
     const today = Date.now();
@@ -12,17 +15,26 @@ const DietScreen = () => {
       console.log(entries);
       setFoodEntries(entries);
     });
-  });
+  }, [logActive]); // reload when switch back
 
   // TODO add dropdown menu with search so dropdown is filled with search results on autocomplete
   return (
-    <View style={styles.container}>
-      {foodEntries.map((entry, index) => (
-        <View key={index}>
-          <Text>{entry.food_name}</Text>
-          <Text>{entry.serving_amount}</Text>
-        </View>
-      ))}
+    <View style={styles.rootContainer}>
+      { logActive ?
+        <DietLogScreen />
+        :
+        <>
+          <View style={styles.container}>
+            {foodEntries.map((entry, index) => (
+              <View key={index}>
+                <Text>{entry.food_name}</Text>
+                <Text>{entry.serving_amount}</Text>
+              </View>
+            ))}
+          </View>
+          <FloatingActionButton onPress={() => {}} />
+        </>
+      }
     </View>
   );
 };
@@ -32,6 +44,9 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
   },
+  rootContainer: {
+    flex: 1,
+  }
 });
 
 export default DietScreen;
---
+++ FloatingActionButton.tsx
@@ -0,0 +1,36 @@
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+interface FloatingActionButtonProps {
+  onPress: () => void;
+}
+
+const FloatingActionButton = ({ onPress }: FloatingActionButtonProps) => (
+  <TouchableOpacity 
+    style={styles.fab}
+    onPress={onPress}
+  >
+    <MaterialCommunityIcons name="plus" size={30} color="white" />
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    right: 20,
+    bottom: 20,
+    backgroundColor: '#007AFF',
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8, // Android shadow
+    shadowColor: '#000', // iOS shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+});
+
+export default FloatingActionButton;
