41fcd4d54081d2b17ef3f52ea622c5ad4cfdd8e8
George Balch <gbalch714@gmail.com>
2024-11-30 22:38:39 -0800

    Display daily log on diet screen

--- DietScreen.tsx
+++ DietScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import FoodEntry from './types/FoodEntry';
 import { getFoodByUnixTime } from './network/food';
 
@@ -10,12 +10,19 @@ const DietScreen = () => {
     const today = Date.now();
     getFoodByUnixTime(today).then(entries => {
       console.log(entries);
+      setFoodEntries(entries);
     });
   });
 
   // TODO add dropdown menu with search so dropdown is filled with search results on autocomplete
   return (
     <View style={styles.container}>
+      {foodEntries.map((entry, index) => (
+        <View key={index}>
+          <Text>{entry.food_name}</Text>
+          <Text>{entry.serving_amount}</Text>
+        </View>
+      ))}
     </View>
   );
 };
