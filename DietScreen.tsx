aca382ca39e210e16934d3b4923ccdf8140e5161
George Balch <gbalch714@gmail.com>
2024-12-01 20:24:09 -0800

    Styling

--- DietScreen.tsx
+++ DietScreen.tsx
@@ -31,16 +31,18 @@ const DietScreen = () => {
         <DietLogScreen setLogActive={setLogActive} />
         :
         <>
-          <View style={styles.container}>
-            <Text>{new Date().toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' })}</Text>
-            <Text>Total Calories: {totalCalories} </Text>
+          <View style={styles.headerContainer}>
+            <Text style={styles.date}>{new Date().toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' })}</Text>
+            <Text style={styles.totalCalories}>Total Calories: {totalCalories} </Text>
+          </View>
+          <ScrollView style={styles.foodEntryList}>
             {foodEntries.map((entry, index) => (
-              <ScrollView key={index}>
+              <View key={index} style={styles.foodEntry}>
                 <Text>{entry.name}</Text>
                 <Text>{entry.macros.calories}</Text>
-              </ScrollView>
+              </View>
             ))}
-          </View>
+          </ScrollView>
           <FloatingActionButton onPress={() => setLogActive(true)} />
         </>
       }
@@ -49,9 +51,27 @@ const DietScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  headerContainer: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  date: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  totalCalories: {
+    fontSize: 16,
+    marginTop: 5,
+  },
+  foodEntryList: {
     flex: 1,
     padding: 10,
+  },
+  foodEntry: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 10,
   },
   rootContainer: {
     flex: 1,
