b7f4bd983e81a96df04be2e165b05b33f48a415b
George Balch <gbalch714@gmail.com>
2024-12-08 08:42:28 -0800

    Add switches

--- screens/Analysis.tsx
+++ screens/Analysis.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Switch } from 'react-native';
 import DataPoint from '../types/DataPoint';
 import ScatterPlot from '../ScatterPlot';
 import { getExerciseNames } from '../network/exercise';
-
+import { convertFromDatabaseFormat } from '../utils';
 interface AnalysisScreenProps {
   navigation: any;
 }
@@ -21,16 +21,43 @@ const AnalysisScreen: React.FC<AnalysisScreenProps> = ({ navigation }) => {
       <Text>Analysis</Text>
       <ScatterPlot
         datasets={datasets}
+        datasetLabels={datasetLabels}
         onDataPointClick={() => {}}
       />
+      <View style={styles.switchesContainer}>
+        {exerciseNames.map((exerciseName) => (
+          <View key={exerciseName} style={styles.switchContainer}>
+            <Text>{convertFromDatabaseFormat(exerciseName)}</Text>
+            <Switch />
+          </View>
+        ))}
+        <View style={styles.switchContainer}>
+          <Text>Weight</Text>
+          <Switch />
+        </View>
+        <View style={styles.switchContainer}>
+          <Text>Supplements</Text>
+          <Switch />
+        </View>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+  switchesContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
 });
 export default AnalysisScreen;
