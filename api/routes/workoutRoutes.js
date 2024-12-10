6ba28a1248c048b4bcf9a88d13a796ee2b7c5f28
George Balch <gbalch714@gmail.com>
2024-12-09 19:34:39 -0800

    Add endpoint for update workout

--- api/routes/workoutRoutes.js
+++ api/routes/workoutRoutes.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import { ObjectId } from 'mongodb';
-
 const router = express.Router();
-
 export default function workoutRoutes(workoutCollection) {
   router.post('/', async (req, res) => {
     try {
@@ -12,7 +10,6 @@ export default function workoutRoutes(workoutCollection) {
       res.status(500).json({ error: 'Failed to create item' });
     }
   });
-
   router.get('/:id', async (req, res) => {
     try {
       const workout = await workoutCollection.findOne({ _id: new ObjectId(req.params.id) });
@@ -25,7 +22,6 @@ export default function workoutRoutes(workoutCollection) {
       res.status(500).json({ error: 'Failed to retrieve workout' });
     }
   });
-
   router.get('/', async (req, res) => {
     try {
       const workouts = await workoutCollection.find({}).toArray();
@@ -34,7 +30,6 @@ export default function workoutRoutes(workoutCollection) {
       res.status(500).json({ error: 'Failed to retrieve workouts' });
     }
   });
-
   router.delete('/:id', async (req, res) => {
     try {
       const result = await workoutCollection.deleteOne({ _id: new ObjectId(req.params.id) });
@@ -43,6 +38,16 @@ export default function workoutRoutes(workoutCollection) {
       res.status(500).json({ error: 'Failed to delete workout' });
     }
   });
-
+  router.put('/:id', async (req, res) => {
+    try {
+      const result = await workoutCollection.updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $set: req.body }
+      );
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to update workout' });
+    }
+  });
   return router;
 }
