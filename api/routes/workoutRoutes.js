4d1ac6c9ac0c47b1f8f23a5d2dc51a58c08489b1
George Balch <gbalch714@gmail.com>
2024-12-10 16:26:18 -0800

    Remove extra workout in url

--- api/routes/workoutRoutes.js
+++ api/routes/workoutRoutes.js
@@ -2,18 +2,18 @@ import express from 'express';
 import { ObjectId } from 'mongodb';
 const router = express.Router();
 export default function workoutRoutes(workoutCollection) {
-  router.post('/workout', async (req, res) => {
+  router.post('/', async (req, res) => {
     try {
-      console.log('POST /workout');
+      console.log('POST /');
       const result = await workoutCollection.insertOne(req.body);
       res.status(201).json(result);
     } catch (err) {
       res.status(500).json({ error: 'Failed to create item' });
     }
   });
-  router.get('/workout/:id', async (req, res) => {
+  router.get('/:id', async (req, res) => {
     try {
-      console.log('GET /workout/:id');
+      console.log('GET /:id');
       const workout = await workoutCollection.findOne({ _id: new ObjectId(req.params.id) });
       if (workout) {
         res.json(workout);
@@ -24,27 +24,27 @@ export default function workoutRoutes(workoutCollection) {
       res.status(500).json({ error: 'Failed to retrieve workout' });
     }
   });
-  router.get('/workout', async (req, res) => {
+  router.get('/', async (req, res) => {
     try {
-      console.log('GET /workout');
+      console.log('GET /');
       const workouts = await workoutCollection.find({}).toArray();
       res.json(workouts);
     } catch (err) {
       res.status(500).json({ error: 'Failed to retrieve workouts' });
     }
   });
-  router.delete('/workout/:id', async (req, res) => {
+  router.delete('/:id', async (req, res) => {
     try {
-      console.log('DELETE /workout/:id');
+      console.log('DELETE /:id');
       const result = await workoutCollection.deleteOne({ _id: new ObjectId(req.params.id) });
       res.status(200).json(result);
     } catch (err) {
       res.status(500).json({ error: 'Failed to delete workout' });
     }
   });
-  router.put('/workout', async (req, res) => {
+  router.put('/', async (req, res) => {
     try {
-      console.log('PUT /workout');
+      console.log('PUT /');
       const { _id, ...updateData } = req.body;  // Separate _id from the rest of the data
       const result = await workoutCollection.updateOne(
         { _id: new ObjectId(_id) },
