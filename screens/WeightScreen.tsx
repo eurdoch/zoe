44c9aacce468bb711a915583aec80104030188b4
George Balch <gbalch714@gmail.com>
2024-12-12 08:01:31 -0800

    Replace global modal with local

--- screens/WeightScreen.tsx
+++ screens/WeightScreen.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput, Button } from 'react-native';
 import FloatingActionButton from '../components/FloatingActionButton';
-import { useModal } from '../modals/ModalContext';
-import AddWeightModal from '../modals/AddWeightModal';
-import { getWeight } from '../network/weight';
-import { mapWeightEntriesToDataPoint } from '../utils';
+import { getWeight, postWeight } from '../network/weight';
+import { mapWeightEntriesToDataPoint, showToastError, showToastInfo } from '../utils';
 import ScatterPlot from '../ScatterPlot';
 import DataPoint from '../types/DataPoint';
+import CustomModal from '../CustomModal';
 
 const WeightScreen = () => {
   const [data, setData] = useState<DataPoint[]>([]);
-  const { showModal } = useModal();
+  const [weight, setWeight] = useState<string>("");
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
 
   const loadData = () => {
     getWeight().then(result => {
@@ -18,13 +18,29 @@ const WeightScreen = () => {
     });
   }
 
+  const handleAddWeight = async (_e: any) => {
+    const parsedWeight = parseFloat(weight);
+    if (!isNaN(parsedWeight)) {
+      const result = await postWeight({
+        value: parsedWeight,
+        createdAt: Math.floor(Date.now() / 1000),
+      });
+      if (result.acknowledged) {
+        showToastInfo('Weight added.');
+        loadData();
+      } else {
+        showToastError('Weight could not be added, try again.');
+      }
+      setModalVisible(false);
+      setWeight("");
+    } else {
+      showToastError('Weight must be a number.')
+    }
+  }
+
   useEffect(() => {
     loadData();
   }, []);
-
-  const handleAddWeight = (_e: any) => {
-    showModal(<AddWeightModal loadData={loadData} />);
-  }
 
   return (
     <View style={styles.container}>
@@ -33,7 +49,15 @@ const WeightScreen = () => {
         onDataPointClick={() => {}}
         zoomAndPanEnabled={false}
       />
-      <FloatingActionButton onPress={handleAddWeight} />
+      <FloatingActionButton onPress={() => setModalVisible(true)} />
+      <CustomModal visible={modalVisible} setVisible={setModalVisible}>
+        <TextInput
+          value={weight}
+          onChangeText={setWeight}
+          placeholder="Enter weight"
+        />
+        <Button title="Add" onPress={handleAddWeight} />
+      </CustomModal>
     </View>
   );
 };
