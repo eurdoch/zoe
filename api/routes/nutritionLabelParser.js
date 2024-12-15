a4d58f22585cf0808a6ef1bdaae239854f19ad71
George Balch <gbalch714@gmail.com>
2024-12-15 02:05:56 -0800

    Prompt adjust

--- api/routes/nutritionLabelParser.js
+++ api/routes/nutritionLabelParser.js
@@ -7,7 +7,7 @@ export default function nutritionLabelParserRoutes() {
   router.post('/', async (req, res) => {
     try {
       const { base64ImageString } = req.body;
-      const prompt = "Return a list of JSON objects of nutritional info each containing keys \"name\", \"amount_per_serving\" and \"unit\".";
+      const prompt = "Return as JSON with keys \"nutritional_info\" and \"serving_size\" where nutritional_info is a list of JSON objects containing keys \"name\", \"amount_per_serving\" and \"unit\".";
       const result = await extractNutritionInfo(base64ImageString, prompt);
       res.status(201).json(result);
     } catch (err) {
