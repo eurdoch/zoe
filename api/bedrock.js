135e4bd2db70c4528fecbb1528f0bbf53d3d357b
George Balch <gbalch714@gmail.com>
2024-12-15 01:22:08 -0800

    Change to anthropic model

--- api/bedrock.js
+++ api/bedrock.js
@@ -4,7 +4,7 @@ const config = {
   AWS_REGION: process.env.AWS_REGION || "us-east-1",
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-  MODEL_ID: "meta.llama3-2-90b-instruct-v1:0"
+  MODEL_ID: "anthropic.claude-3-sonnet-20240229-v1:0",
 };
 
 class BedrockService {
