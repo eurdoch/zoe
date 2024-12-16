b79c01dc96047fc95f0d8441ca8e6b962baf655a
George Balch <gbalch714@gmail.com>
2024-12-15 16:28:16 -0800

    asdfasd

--- api/index.js
+++ api/index.js
@@ -45,7 +45,7 @@ async function connectToDatabase() {
     app.get('/', (req, res) => {
       res.send('Ping a da pong');
     });
-    app.get('/tester', (req, res) => {
+    app.get('/testerino', (req, res) => {
       res.send('Ping a da pong');
     });
 
