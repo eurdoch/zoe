57253bcb0188b7b490ca43c167ab18450970ff2d
George Balch <gbalch714@gmail.com>
2025-01-14 15:04:33 -0800

    Add Realm package

--- network/workout.ts
+++ network/workout.ts
@@ -1,6 +1,8 @@
 import Workout from "../types/Workout";
 import WorkoutEntry from "../types/WorkoutEntry";
+
 const VITALE_BOX_URL = "https://directto.link";
+
 export async function postWorkout(workout: Workout): Promise<any> {
   const response = await fetch(`${VITALE_BOX_URL}/workout`, {
     method: 'POST',
@@ -11,24 +13,28 @@ export async function postWorkout(workout: Workout): Promise<any> {
   });
   return response.json();
 }
+
 export async function getWorkout(id: string): Promise<WorkoutEntry> {
   const response = await fetch(`${VITALE_BOX_URL}/workout/${id}`, {
     method: 'GET',
   });
   return response.json();
 }
+
 export async function getWorkouts(): Promise<WorkoutEntry[]> {
   const response = await fetch(`${VITALE_BOX_URL}/workout`, {
     method: 'GET',
   });
   return response.json();
 }
+
 export async function deleteWorkout(id: string): Promise<any> {
   const response = await fetch(`${VITALE_BOX_URL}/workout/${id}`, {
     method: 'DELETE',
   });
   return response.json();
 }
+
 export async function updateWorkout(workoutEntry: WorkoutEntry): Promise<any> {
   const response = await fetch(`${VITALE_BOX_URL}/workout`, {
     method: 'PUT',
