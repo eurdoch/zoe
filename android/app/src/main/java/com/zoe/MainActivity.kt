080c7d74ac3df830d2d1d4e28c3607a4a1ba065c
George Balch <gbalch714@gmail.com>
2025-01-14 14:42:16 -0800

    Change android app name

--- android/app/src/main/java/com/zoe/MainActivity.kt
+++ android/app/src/main/java/com/zoe/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "vitale"
+  override fun getMainComponentName(): String = "zoe"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
