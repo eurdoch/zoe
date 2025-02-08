f2920de823f5ed4221f343d3c105c1709c8aad72
George Balch <gbalch714@gmail.com>
2025-02-07 18:29:51 -0800

    Update install script for iphone

---
+++ README.md
@@ -0,0 +1,17 @@
+# hey FriedKielbasa!
+
+if you want to just try this out, make sure iphone is connected to macbook and xcode command line tools are setup on macbook
+```
+git clone https://github.com/eurdoch/zoe.git
+cd zoe
+yarn
+yarn ios (or yarn android)
+```
+
+to install 
+```
+git clone https://github.com/eurdoch/zoe.git
+cd zoe
+yarn
+./install_to_phone.sh
+```
--- install_to_phone.sh
+++ install_to_phone.sh
@@ -6,13 +6,27 @@ select_device() {
     devices=($(adb devices | grep -v "List" | grep "device$" | cut -f1))
     
     if [ ${#devices[@]} -eq 0 ]; then
-        echo "No devices connected!"
-        exit 1
+        echo "No Android devices connected!"
+        echo "Checking for iOS devices..."
+        ios_devices=($(system_profiler SPUSBDataType | grep "iPhone" | awk '{print $6}' | sed 's/://'))
+        if [ ${#ios_devices[@]} -eq 0 ]; then
+            echo "No iOS devices connected either!"
+            exit 1
+        else
+            echo "iOS devices found: ${ios_devices[@]}"
+            select ios_device in "${ios_devices[@]}"; do
+                if [ -n "$ios_device" ]; then
+                    selected_device=$ios_device
+                    is_ios=true
+                    break
+                fi
+            done
+        fi
     elif [ ${#devices[@]} -eq 1 ]; then
-        echo "Only one device connected. Using ${devices[0]}"
+        echo "Only one Android device connected. Using ${devices[0]}"
         selected_device=${devices[0]}
     else
-        echo "Multiple devices found. Please select a device:"
+        echo "Multiple Android devices found. Please select a device:"
         select device in "${devices[@]}"; do
             if [ -n "$device" ]; then
                 selected_device=$device
@@ -32,11 +46,23 @@ cd android
 cd ..
 
 # Bundle the app
-npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+if [ "$is_ios" = true ]; then
+    npx react-native bundle --platform ios --dev false --entry-file index.js --bundle-output ios/main.jsbundle --assets-dest ios
+else
+    npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+fi
 
-cd android
-./gradlew assembleDebug
+if [ "$is_ios" = true ]; then
+    cd ios
+    xcodebuild -scheme YourAppName
+    cd ..
+    open ios/build/Build/Products/Debug-iphoneos/YourAppName.app
+else
+    cd android
+    ./gradlew assembleDebug
 
-# Select device and install
-select_device
-adb -s $selected_device install app/build/outputs/apk/debug/app-debug.apk
+    # Select device and install
+    select_device
+    adb -s $selected_device install app/build/outputs/apk/debug/app-debug.apk
+fi
+
