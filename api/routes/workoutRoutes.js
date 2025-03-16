49931366130a76d8c0efaef9fbfd027128c8ab7c
George Balch <gbalch714@gmail.com>
2025-03-16 12:22:13 -0700

    Typo

--- api/routes/workoutRoutes.js
+++ api/routes/workoutRoutes.js
@@ -38,7 +38,7 @@ export default function workoutRoutes(workoutCollection) {
   router.delete('/:id', async (req, res) => {
     try {
       console.log(`DELETE /workout/:id`);
-      const result = await workoutCollection.deleteOne({ _id: req.params.id) });
+      const result = await workoutCollection.deleteOne({ _id: req.params.id });
       res.status(200).json(result);
     } catch (err) {
       res.status(500).json({ error: 'Failed to delete workout' });
