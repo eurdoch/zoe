52aa9569cfda7493569fd01f08d0848bc74a4beb
George Balch <george.balch@proton.me>
2025-04-19 23:47:32 +0000

    Remove web build for now

--- api/index.js
+++ api/index.js
@@ -74,7 +74,9 @@ async function connectToDatabase() {
       }
 
       if (req.body.ref === 'refs/heads/master') {
-        exec(`cd ${repoPath} && git pull origin master && yarn web:build && pm2 reload zoe`, (error, stdout, stderr) => {
+	      // TODO figure out where web:build is needed
+        exec(`cd ${repoPath} && git pull origin master pm2 reload zoe`, (error, stdout, stderr) => {
+        //exec(`cd ${repoPath} && git pull origin master && yarn web:build && pm2 reload zoe`, (error, stdout, stderr) => {
           if (error) {
             console.error(`Error: ${error}`);
             return res.status(500).send(error);
