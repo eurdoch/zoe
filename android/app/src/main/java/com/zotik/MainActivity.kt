a4de9e74539493704e93e7884956271ae4f19411
George Balch <gbalch714@gmail.com>
2025-04-25 13:34:18 -0700

    App rename

--- android/app/src/main/java/com/zotik/MainActivity.kt
+++ android/app/src/main/java/com/zotik/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "zotik"
+  override fun getMainComponentName(): String = "Kallos"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
